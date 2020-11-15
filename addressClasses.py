34d3c5202d0fb09892dc6a474533be75f1fe703a
Jafet Suárez <jafet.suarezxbox@hotmail.com>
2020-11-15 17:32:03 -0600

    app

--- addressClasses.py
+++ addressClasses.py
@@ -1,15 +1,15 @@
 def ipAddressClass(ip):
     ip=int(ip.split('.')[0])
     if(1<=ip and ip<=127):
-            print("Clase A")
+        return("Clase A")
     elif(128<=ip and ip<=191):
-        print("Clase B")
+        return("Clase B")
     elif(192<=ip and ip<=223):
-        print("Clase C")
+        return("Clase C")
     elif(224<=ip and ip<=239):
-        print("Clase D")
+        return("Clase D")
     elif(240<=ip and ip<=255):
-        print("Clase E")
+        return("Clase E")
 
 
 if(__name__=="__main__"):
