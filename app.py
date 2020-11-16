bf7b1d0a93adf055ab7f157e5d38ae4fb617f8cc
Jafet Suárez <jafet.suarezxbox@hotmail.com>
2020-11-15 23:59:07 -0600

    un solo route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import subnetMask as sm
 import difusion as df
 import segmentos as nh
 import  networkAddress as na
+import flIP as fl
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
@@ -12,40 +13,24 @@ app.config.from_object(DevelopmentConfig)
 def page_not_found(e): #recibe como parametro el error
     return render_template('404.html'),404
 
-@app.route('/ip-address-class/<x1>')
-def ip_address_class(x1=None):
-	if x1:
-		data = {'ipaddressclass':ac.ipAddress(x1)}
-		response = app.response_class(
-			response=json.dumps(data),
-			status=200,
-			mimetype='application/json'
-		)
-		return response
-	
-@app.route('/subnet-mask/<x1>')
-@app.route('/subnet-mask/<int:x2>')
+@app.route('/ipv4-calc/<x1>')
+@app.route('/ipv4-calc/<x1>/<int:x2>')
+@app.route('/ipv4-calc/<int:x2>')
 def prefix_to_mask(x1=None, x2=None):
+	data={}
 	if x1:
-		return jsonify({'masktoprefix':sm.maskToPrefix(x1)})
+		data['ipaddressclass']=ac.ipAddressClass(x1)
+		data['defaultmask']=ac.ipAddressMask(x1)
+		data['networkhost']=nh.networkHost(x1)
+		data['firstdirecction']=fl.calcFirstIP(x1)
+		data['lastdirecction']=fl.calcLastIP(x1)
+		data['networkaddress']=na.networkAddress(x1)
+	if x1 and x2:
+		data['subnetworkdirection']=fl.calcRed(x1,x2)
+		data['ipaddressdifusion']=fl.calcDifusion(x1,x2)
 	if x2:
-		return jsonify({'prefixtomask':sm.prefixToMask(x2)})
-
-@app.route('/difusion/<x1>')
-def ip_difusion(x1=None):
-	if x1:
-		return jsonify({'ipaddressdifusion':df.difusionIp(x1)})
-
-@app.route('/network-host/<x1>')
-def network_host(x1=None):
-	if x1:
-		return jsonify({'networkhost':nh.networkHost(x1)})		
-
-@app.route('/network-address/<x1>')
-def network_address(x1=None):
-	if x1:
-		return jsonify({'networkaddress':na.networkAddress(x1)})		
-
+		data['prefixtomask']=sm.prefixToMask(x2)
+	return jsonify(data)		
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8999)
